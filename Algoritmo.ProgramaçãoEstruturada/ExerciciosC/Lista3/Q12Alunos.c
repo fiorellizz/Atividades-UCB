ac650ca2d323f58c9f062994cb8454349a65c538
Guilherme Fiorelli Braz Rodrigues <135390306+fiorellizz@users.noreply.github.com>
2023-10-02 15:46:08 -0300

    Create Q12Alunos.c

---
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q12Alunos.c
@@ -0,0 +1,2 @@
+//Crie um programa em linguagem C que permita entrar com o nome, a nota da prova 1 e da prova 2 de 15 alunos. Ao final, imprimir uma listagem, contendo: nome, nota da prova 1, nota da prova 2, e média das notas de cada aluno. Ao final, imprimir a média geral da turma.
+
