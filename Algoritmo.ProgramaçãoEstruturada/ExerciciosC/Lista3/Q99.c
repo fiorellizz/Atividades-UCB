47be5d9b3c7ececa1de16872e31ffe2b0ed05fa0
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-10-02 15:06:54 -0300

    Update Q9.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q99.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q99.c
@@ -1,29 +1,26 @@
 //Faça um programa em C que permita entrar com o nome, a idade e o sexo de 20 pessoas. O programa deve imprimir o nome da pessoa se ela for do sexo masculino e tiver mais de 21 anos.
 
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
+
     char nome[50];
-    int idade;
-    char sexo;
+    int idade, sexo;
 
     for (int i = 0; i < 5; i++){  
         printf("Pessoa %d\n", i + 1);
         
         printf("Digite o nome da pessoa: ");
-        gets(nome); 
+        scanf("%s", &nome);
         
         printf("Digite a idade da pessoa: ");
         scanf("%d", &idade);
 
-        printf("Digite o sexo da pessoa (M/F): ");
-        scanf("%c", &sexo); 
+        printf("Digite o sexo da pessoa :\n1 - MASCULINO\n2 - FEMININO\n");
+        scanf("%d", &sexo); 
 
-        sexo = toupper(sexo);
-
-        if (idade >= 21 && sexo == 'M') {
-            printf("%s tem mais de 21 anos e é do sexo M\n", nome);
+        if (idade >= 21 && sexo == 1) {
+            printf("%s tem mais de 21 anos e é do sexo MASCULINO\n", nome);
         }
     }
 
