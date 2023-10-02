747a35c4e488ea317e4553e97f47f94a79589bc0
Guilherme Fiorelli Braz Rodrigues <135390306+fiorellizz@users.noreply.github.com>
2023-10-02 15:44:09 -0300

    Update Q10Fibonacci.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q10Fibonacci.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q10Fibonacci.c
@@ -1,3 +1,8 @@
+/* Crie um programa em C que imprima os 20 primeiros termos da série de Fibonacci.
+Observação: os dois primeiros termos desta série são 1 e 1 e os demais são gerados a partir da soma dos anteriores.
+Exemplo:
+• 1 + 1 = 2, terceiro termo;
+• 1 + 2 = 3, quarto termo, etc. */
 
 #include <stdio.h>
 
