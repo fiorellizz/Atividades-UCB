9df5a851e1e167e6315df0284c5e29f5d84d98e4
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-10-16 14:32:46 -0300

    Update Q14Voleibol.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q14Voleibol.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q14Voleibol.c
@@ -9,53 +9,46 @@
 #include <conio.h>
 #include <stdlib.h>
 
-int main()
+int jogadores = 12, times = 2;
+float peso[12], idade[12], pesado = 0, jovem = 200, pesoMedioTime = 0, idadeMediaTime = 0, pesoTotal = 0, idadeTotal = 0;
+
+void ColetarIdadesPeso()
 {
-    system("cls");
-    int jogadores = 12, times = 2;
-    float peso[12], idade[12], pesado = 0, jovem = 200, pesoMedioTime = 0, idadeMediaTime = 0, pesoTotal = 0, idadeTotal = 0;
-
-    for (int i = 0; i < times; i++) // rodar para os 30 times
+    for (int i = 0; i < jogadores; i++) // coletar idades e peso de cada time
     {
-        printf("DIGITE OS DADOS DO %d° TIME\n", i + 1);
-        for (int i = 0; i < jogadores; i++) // coletar idades e peso de cada time
-        {
-            printf("Digite o peso do %d° jogador: ", i + 1);
-            scanf("%f", &peso[i]);
-            printf("Digite a idade do %d° jogador: ", i + 1);
-            scanf("%f", &idade[i]);
-            pesoMedioTime = pesoMedioTime + peso[i];
-            idadeMediaTime = idadeMediaTime + idade[i];
-            pesoTotal = pesoTotal + peso[i];
-            idadeTotal = idadeTotal + idade[i];
-        }
+        printf("Digite o peso do %d° jogador: ", i + 1);
+        scanf("%f", &peso[i]);
+        printf("Digite a idade do %d° jogador: ", i + 1);
+        scanf("%f", &idade[i]);
+        pesoMedioTime += peso[i];
+        idadeMediaTime += idade[i];
+        pesoTotal += peso[i];
+        idadeTotal += idade[i];
+    }
+}
 
-        for (int i = 0; i < jogadores; i++) // passar por todos os vetores e pegar o maior peso
+void Pesado(){
+    for (int i = 0; i < jogadores; i++) // passar por todos os vetores e pegar o maior peso
         {
             if (peso[i] > pesado)
             {
                 pesado = peso[i];
             }
         }
+}
 
-        for (int i = 0; i < jogadores; i++) // passar por todos os vetores e pegar a menor idade
+void Novo(){
+    for (int i = 0; i < jogadores; i++) // passar por todos os vetores e pegar a menor idade
         {
             if (idade[i] < jovem)
             {
                 jovem = idade[i];
             }
         }
+}
 
-        pesoMedioTime = pesoMedioTime / jogadores;
-        idadeMediaTime = idadeMediaTime / jogadores;
-        system("cls");
-        printf("RELATORIO DO %d° TIME\n", i + 1);
-        printf("A media de peso do time é de: %.2f kilos\n", pesoMedioTime);
-        printf("A media de idade do time é de: %.1f anos\n", idadeMediaTime);
-        printf("O atleta mais pesado do time tem: %.2f kilos\n", pesado);
-        printf("O atleta mais novo do time tem: %.0f anos\n", jovem);
-
-        for (int i = 0; i < jogadores; i++) // zerando os dados para usar para outros jogadores
+void ZerarDados(){
+    for (int i = 0; i < jogadores; i++) // zerando os dados para usar para outros jogadores
         {
             peso[i] = 0;
             idade[i] = 0;
@@ -64,12 +57,34 @@ int main()
         jovem = 200;
         pesoMedioTime = 0;
         idadeMediaTime = 0;
+}
 
+void TodosTimes(){
+    for (int i = 0; i < times; i++) // rodar para os 30 times
+    {
+        printf("DIGITE OS DADOS DO %d° TIME\n", i + 1);
+        ColetarIdadesPeso();
+        Pesado();
+        Novo();
+        pesoMedioTime /= jogadores;
+        idadeMediaTime /= jogadores;
+        system("cls");
+        printf("RELATORIO DO %d° TIME\n", i + 1);
+        printf("A media de peso do time é de: %.2f kilos\n", pesoMedioTime);
+        printf("A media de idade do time é de: %.1f anos\n", idadeMediaTime);
+        printf("O atleta mais pesado do time tem: %.2f kilos\n", pesado);
+        printf("O atleta mais novo do time tem: %.0f anos\n", jovem);
+        ZerarDados();
         printf("Digite qualquer tecla para continuar...");
         getch();
         system("cls");
     }
+}
 
+int main()
+{
+    system("cls");
+    TodosTimes();
     /*peso e idade media total*/
     pesoTotal = pesoTotal / (jogadores * times);
     idadeTotal = idadeTotal / (jogadores * times);
