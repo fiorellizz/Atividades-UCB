6c53cc82e48c0324fb56c7bcaf5c8a03de762588
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-10-17 16:26:01 -0300

    Create Q25Mercadorias.c

---
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q25Mercadorias.c
@@ -0,0 +1,13 @@
+/*Construa um programa que receba o valor e o código de várias mercadorias vendidas em um determinado dia. Os códigos obedecem a lista a seguir:
+
+L-limpeza
+A-Alimentação
+H-Higiene
+
+Calcule e imprima:
+
+• o total vendido naquele dia, com todos os códigos juntos;
+• o total vendido naquele dia em cada um dos códigos.
+
+Obs.: Para encerrar a entrada de dados, digite o valor da mercadoria zero.*/
+
