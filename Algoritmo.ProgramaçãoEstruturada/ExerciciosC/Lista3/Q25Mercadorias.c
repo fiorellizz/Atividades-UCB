d7d1c69bce8223ab23679a26fd13cfffd023539f
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-10-20 11:53:19 -0300

    Update Q25Mercadorias.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q25Mercadorias.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Lista3/Q25Mercadorias.c
@@ -11,3 +11,65 @@ Calcule e imprima:
 
 Obs.: Para encerrar a entrada de dados, digite o valor da mercadoria zero.*/
 
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+
+    char codigo;
+    float valorProduto[3] = {0}, valor;
+    int quantidade[3] = {0};
+
+    system("cls");
+
+    printf("Digite o valor da mercadoria: ");
+    scanf("%f", &valor);
+
+    while (valor != 0)
+    {
+        printf("Digite o codigo da merdadora: ");
+        scanf("%s", &codigo);
+
+        codigo = toupper(codigo);
+
+        switch (codigo)
+        {
+        case 'L':
+            quantidade[0]++;
+            valorProduto[0] += valor;
+            break;
+
+        case 'A':
+            quantidade[1]++;
+            valorProduto[1] += valor;
+            break;
+
+        case 'H':
+            quantidade[2]++;
+            valorProduto[2] += valor;
+            break;
+
+        default:
+            break;
+        }
+
+        printf("Dados armazenados!\n");
+        printf("Digite qualquer tecla para continuar...");
+        getch();
+        system("cls");
+        printf("Digite o valor proxima mercadoria: ");
+        scanf("%f", &valor);
+    }
+
+    system("cls");
+    printf("RELATORIO FINAL\n");
+    printf("Foram vendidas %d mercadorias no dia e o total arrecadado foi de R$%.2f\n", quantidade[0] + quantidade[1] + quantidade[2], valorProduto[0] + valorProduto[1] + valorProduto[2]);
+    printf("Foram vendidas %d mercadoria de Limpeza, totalizando R$%.2f\n", quantidade[0], valorProduto[0]);
+    printf("Foram vendidas %d mercadoria de Alimentação, totalizando R$%.2f\n", quantidade[1], valorProduto[1]);
+    printf("Foram vendidas %d mercadoria de Higiene, totalizando R$%.2f\n", quantidade[2], valorProduto[2]);
+
+    return 0;
+}
