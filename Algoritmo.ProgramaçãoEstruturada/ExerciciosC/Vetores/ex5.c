da05679853f30686bac51059519d8b4d2d50dd3e
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-11-01 23:15:53 -0300

    Update ex5.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Vetores/ex5.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/Vetores/ex5.c
@@ -6,8 +6,11 @@
 #include <stdlib.h>
 #include <conio.h>
 
-int main() {
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
+
+    system("cls");
 
     int alunos;
     printf("Quantos alunos você vai calcular a média: ");
@@ -16,7 +19,8 @@ int main() {
     float provas1[alunos], provas2[alunos], media[alunos];
     char nome[alunos][50], situacao[alunos][10];
 
-    for (int i = 0; i < alunos; i++) {
+    for (int i = 0; i < alunos; i++)
+    {
         printf("Digite o nome do %d° aluno: ", i + 1);
         scanf("%s", nome[i]);
 
@@ -25,20 +29,33 @@ int main() {
 
         printf("Digite a nota da prova 2 do aluno: ");
         scanf("%f", &provas2[i]);
+
+        system("cls");
     }
 
-    for (int i = 0; i < alunos; i++) {
+    for (int i = 0; i < alunos; i++)
+    {
         media[i] = (provas1[i] + provas2[i]) / 2;
-        if (media[i] < 7) {
+        if (media[i] < 7)
+        {
             strcpy(situacao[i], "Reprovado");
-        } else {
+        }
+        else
+        {
             strcpy(situacao[i], "Aprovado");
         }
     }
 
-    for (int i = 0; i < alunos; i++) {
-        printf("Média do(a) aluno(a) %s foi de %.0f e está %s.\n", nome[i], media[i], situacao[i]);
+    printf("\n-----------------------------------------------------------------\n");
+    printf("|\t\t\t TABELA DA SALA \t\t\t|\n");
+    printf("-----------------------------------------------------------------\n");
+    printf("|\tNOME\t\t|\tMEDIA\t|\tSITUACAO\t|\n");
+    printf("-----------------------------------------------------------------\n");
+    for (int i = 0; i < alunos; i++)
+    {
+        printf("|\t%s\t\t|\t%.0f\t|\t%s\t|\n", nome[i], media[i], situacao[i]);
     }
+    printf("-----------------------------------------------------------------\n");
 
     return 0;
 }
