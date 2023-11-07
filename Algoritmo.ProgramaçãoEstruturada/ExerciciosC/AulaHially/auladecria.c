1031055184eaad00508b589fa7220bb07cd52e4c
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-11-07 12:29:36 -0300

    Update auladecria.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/AulaHially/auladecria.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/AulaHially/auladecria.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 #define MAX 3
 
-typedef struct
-{
+typedef struct{
     char nome[20];
     int idade, id;
 }Aluno;
 
-void cadastrar(Aluno a[], int indice)
-{
+void cadastrar(Aluno a[], int indice){
     printf("Digite o nome do aluno: ");
     fflush(stdin);
     fgets(a[indice].nome, sizeof(a[indice].nome), stdin);
@@ -21,70 +21,58 @@ void cadastrar(Aluno a[], int indice)
     scanf("%d", &a[indice].id);
 }
 
-void listarAlunos(Aluno a[], int indice)
-{
-    if (indice == 0)
-    {
+void listarAlunos(Aluno a[], int indice){
+    if (indice == 0){
         printf("Não existem registros na lista!");
-    }
-    else
-    {
-        for (int i = 0; i < indice; i++)
-        {
-            printf("\nNome: %s", a[i].nome);
+    }else{
+        for (int i = 0; i < indice; i++){
+            printf("Nome: %s", a[i].nome);
             printf("Idade: %d\n", a[i].idade);
+            printf("\n");
         }
     }
 }
 
-void buscar(Aluno a[], int indice, int idbusca)
-{
-    int encontrado;
-    for (int i = 0; i < indice; i++)
-    {
-        if (idbusca == a[i].id)
-        {
+void buscar(Aluno a[], int indice, int idbusca){
+    int encontrado = 0;
+    for (int i = 0; i < indice; i++){
+        if (idbusca == a[i].id){
             printf("\nNome: %s", a[i].nome);
             printf("Idade: %d\n", a[i].idade);
             encontrado++;
         }
     }
-
-    if (encontrado == 0)
-    {
-        printf("Registro não escontrado!");
+    if (encontrado == 0){
+        printf("Registro não escontrado!\n");
     }
 }
 
-int main()
-{
-
+int main(){
     Aluno a[MAX];
     int count = 0, opcao, idbusca;
-
-    do
-    {
-        printf("\nDigite 1 para cadastrar");
+    do{
+        printf("Digite 1 para cadastrar");
         printf("\nDigite 2 para listar");
         printf("\nDigite 3 para buscar");
         printf("\nDigite 0 para sair");
         printf("\nDigite a opcao escolhida: ");
         scanf("%d", &opcao);
+        system("cls");
 
-        switch (opcao)
-        {
+        switch (opcao){
         case 1:
-            if (count < MAX)
-            {
+            if (count < MAX){
                 cadastrar(a, count);
                 count++;
-                printf("Cadastro realizado com sucesso!");
-                printf("\n");
-            }
-            else
-            {
-                printf("Não foi possivel realizar o cadastro!");
-                printf("\n");
+                printf("Cadastro realizado com sucesso!\n");
+                printf("Digite qualquer tecla para continuar...");
+                getch();
+                system("cls");
+            }else{
+                printf("Não foi possivel realizar o cadastro!\n");
+                printf("Digite qualquer tecla para continuar...");
+                getch();
+                system("cls");
                 // opcao = 0;
             }
             break;
