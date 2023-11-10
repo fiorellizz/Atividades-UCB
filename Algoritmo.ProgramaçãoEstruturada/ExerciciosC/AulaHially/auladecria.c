49bdc236bbae913c15bb8bc56e4d174ef2e3c2db
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2023-11-10 15:21:57 -0300

    Update auladecria.c

--- Algoritmo.ProgramaçãoEstruturada/ExerciciosC/AulaHially/auladecria.c
+++ Algoritmo.ProgramaçãoEstruturada/ExerciciosC/AulaHially/auladecria.c
@@ -19,11 +19,11 @@ void cadastrar(Produto a[], int indice)
     fgets(a[indice].nome, sizeof(a[indice].nome), stdin);
     a[indice].nome[strcspn(a[indice].nome, "\n")] = '\0';
 
-    printf("Digite a quantidade de produto que serão armazenadas no estoque: ");
+    printf("Digite a quantidade do produto: ");
     fflush(stdin);
     scanf("%d", &a[indice].quantidade);
 
-    printf("Digite o lote do produto que será armazenada no estoque: ");
+    printf("Digite o lote do produto: ");
     fflush(stdin);
     scanf("%d", &a[indice].lote);
 }
@@ -62,21 +62,92 @@ void buscar(Produto a[], int indice, int idbusca)
     }
 }
 
+void alterar(Produto a[], int indice, int idbusca){
+    int mudar;
+    for (int i = 0; i < indice; i++)
+    {
+        if (idbusca == a[i].id){
+            printf("\nID: %d", a[i].id);
+            printf("\nNome: %s", a[i].nome);
+            printf("\nQuantidade: %d", a[i].quantidade);
+            printf("\nLote: %d", a[i].lote);
+            printf("\n\n");
+            printf("1 - Alterar nome \n2 - Alterar quantidade \n3 - Alterar status \n4 - Alterar lote \n5 - Alterar todos \n");
+            fflush(stdin);
+            printf("Digite a opção desejada: ");
+            fflush(stdin);
+            scanf("%d", &mudar);
+            fflush(stdin);
+            if (mudar == 1){
+                printf("Digite o novo nome: ");
+                fgets(a[i].nome, sizeof(a[i].nome), stdin);
+                a[indice].nome[strcspn(a[indice].nome, "\n")] = '\0';
+                fflush(stdin);
+            }else if(mudar == 2){
+                printf("Digite a nova quantidade:");
+                scanf("%d", &a[i].quantidade);
+                fflush(stdin);
+            }else if(mudar == 3){
+                printf("Digite o novo status (0 ou 1): ");
+                scanf("%d", &a[i].status);
+                fflush(stdin);
+            }else if(mudar == 4){
+                printf("Digite o novo lote: ");
+                scanf("%d", &a[i].lote);
+                fflush(stdin);
+            }else if(mudar == 5){
+                printf("Digite o novo nome: ");
+                fgets(a[i].nome, sizeof(a[i].nome), stdin);
+                a[indice].nome[strcspn(a[indice].nome, "\n")] = '\0';
+                printf("Digite a nova quantidade:");
+                scanf("%d", &a[i].quantidade);
+                printf("Digite o novo status (0 ou 1): ");
+                scanf("%d", &a[i].status);
+                printf("Digite o novo lote: ");
+                scanf("%d", &a[i].lote);
+                fflush(stdin);
+            }else{
+                printf("Opção invalida!");
+            }
+            return;
+        }
+        
+    }
+    
+}
+
+void excluir(Produto a[], int indice, int idbusca){
+    for(int i = 0; i < indice; i++){
+        if (idbusca == a[i].id){
+            for(int j = i; j < indice - 1; j++){
+                a[j] = a[j + 1];
+            }
+            printf("Excluido com sucesso!");
+            return;
+        }
+    }
+    printf("Registro nao encontrado!");
+}
+
 int main()
 {
     Produto a[MAX];
-    int count = 0, opcao, idbusca;
+    int count = 0, mudar, idbusca;
+    char exclui;
+
     do
     {
-        printf("Digite 1 para cadastrar");
-        printf("\nDigite 2 para listar");
-        printf("\nDigite 3 para buscar");
-        printf("\nDigite 0 para sair");
-        printf("\nDigite a opcao escolhida: ");
-        scanf("%d", &opcao);
+        printf("[1] Cadastrar");
+        printf("\n[2] Listar");
+        printf("\n[3] Buscar");
+        printf("\n[4] Alterar");
+        printf("\n[5] Excluir");
+        printf("\n[0] Sair");
+        printf("\nDigite a opção escolhida: ");
+        scanf("%d", &mudar);
         system("cls");
 
-        switch (opcao)
+        switch (mudar)
         {
         case 1:
             if (count < MAX)
@@ -94,7 +165,7 @@ int main()
                 printf("Digite qualquer tecla para voltar ao menu...");
                 getch();
                 system("cls");
-                // opcao = 0;
+                // mudar = 0;
             }
             break;
 
@@ -109,15 +180,33 @@ int main()
             buscar(a, count, idbusca);
             break;
 
+        case 4:
+            printf("Digite o id que deseja alterar: ");
+            scanf("%d", &idbusca);
+            alterar(a, count, idbusca);
+            break;
+        
+        case 5:
+            printf("Digite o id que deseja excluir: ");
+            scanf("%d", &idbusca);
+            fflush(stdin);
+            printf("Deseja realmente excluir (s / n) ?\n");
+            scanf("%c", &exclui);
+                if (exclui == 's'){
+                    excluir(a, count, idbusca);
+                    count--;
+                    printf("\n--------------------------\n");
+                }else{
+                    printf("Exclusao Cancelada!");
+                    printf("\n--------------------------\n");
+                }
+            break;
+
         default:
-            if (opcao != 0)
-            {
-                printf("Valor inválido!\n");
-            }
             break;
         }
 
-    } while (opcao != 0);
+    } while (mudar != 0);
 
     return 0;
 }
