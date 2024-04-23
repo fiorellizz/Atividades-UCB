ea482c3cce145966acbedbe56cafc09f4fba12b4
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2024-04-23 20:18:56 -0300

    add prova.c

--- Estrutura de dados/Ex em sala/prova3.c
+++ Estrutura de dados/Ex em sala/prova3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 typedef struct Pessoa {
-    char nome [1024];
+    char nome[1024];
     int idade;
-    float altura = 1.75;
-    float peso = 70;
-};
+    float altura;
+    float peso;
+} Pessoa; // Adicionando um alias "Pessoa" para a struct
 
-//IMC = peso/(altura x altura)
-float calcularIMC (struct Pessoa *X) {
-    float IMC = X->peso/(X->altura * X->altura);
-    return(IMC);
+// IMC = peso / (altura x altura)
+float calcularIMC(Pessoa *X) {
+    float IMC = X->peso / (X->altura * X->altura);
+    return IMC;
 }
 
-int main(){
-    struct Pessoa X[1];
-    
-    float imc = calcularIMC(X);
+int main() {
+    Pessoa X;
+    X.altura = 1.75;
+    X.peso = 70;
+
+    float imc = calcularIMC(&X);
     printf("%f", imc);
 
     return 0;
