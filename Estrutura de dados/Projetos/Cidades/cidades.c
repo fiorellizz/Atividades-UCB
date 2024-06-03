533ef3d00efeefa6ee520b659876d85a7df8c376
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2024-06-03 14:22:03 -0300

    add projet cidades

---
+++ Estrutura de dados/Projetos/Cidades/cidades.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cidades.h"
+
+Estrada *getEstrada(const char *nomeArquivo){
+    
+} // Inicializa cidades no TAD Cidade.
+
+double calcularMenorVizinhanca(const char *nomeArquivo){
+    
+} // Retorna a menor vizinhança.
+
+char *cidadeMenorVizinhanca(const char *nomeArquivo){
+    
+} // Retorna a cidade que tem menor vizinhança.
---
+++ Estrutura de dados/Projetos/Cidades/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cidades.h"
+#include "cidades.c"
+
+int main(){
+
+
+
+    return 0;
+}
