377e28986c4b9de5e5e4656f33e583566169bbfa
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2024-06-16 19:52:17 -0300

    att code return arquivo

--- EstruturaDados/Projetos/Calculadora/main.c
+++ EstruturaDados/Projetos/Calculadora/main.c
@@ -4,17 +4,49 @@
 #include <math.h>
 #include "calculadora.h"
 
-int main() {
+void executarTeste(char *posFixa, int numeroTeste) {
+    FILE *arquivo = fopen("Resultado.txt", "a");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
 
-    char posFixa[] = "53 23 + 8 2 - *";
-    
-    // Convertendo para a forma infixa
-
+    fprintf(arquivo, "TESTE %d:\n", numeroTeste);
     char *inFixa = getFormaInFixa(posFixa);
-    
-    // Calculando o valor da expressão
-
+    fprintf(arquivo, "Forma infixa: %s\n", inFixa);
     float valor = getValor(posFixa);
-    
+    fprintf(arquivo, "Valor da expressão: %.2f\n\n", valor);
+
+    fclose(arquivo);
+}
+
+int main() {
+    char posFixa01[] = "3 4 + 5 *";
+    executarTeste(posFixa01, 1);
+
+    char posFixa02[] = "7 2 * 4 +";
+    executarTeste(posFixa02, 2);
+
+    char posFixa03[] = "8 5 2 4 + * +";
+    executarTeste(posFixa03, 3);
+
+    char posFixa04[] = "6 2 / 3 + 4 *";
+    executarTeste(posFixa04, 4);
+
+    char posFixa05[] = "9 5 2 8 * 4 + * +";
+    executarTeste(posFixa05, 5);
+
+    char posFixa06[] = "2 3 + log 5 /";
+    executarTeste(posFixa06, 6);
+
+    char posFixa07[] = "10 log 3 ^ 2 +";
+    executarTeste(posFixa07, 7);
+
+    char posFixa08[] = "45 60 + 30 cos *";
+    executarTeste(posFixa08, 8);
+
+    char posFixa09[] = "0.5 45 sen 2 ^ +";
+    executarTeste(posFixa09, 9);
+
     return 0;
 }
